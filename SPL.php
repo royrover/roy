bea20a727816d9ea0c41fab99086084f78c8a4cc
royrover <60468002+royrover@users.noreply.github.com>
2024-09-04 17:44:53 +0700

    Update SPL.php

--- SPL.php
+++ SPL.php
@@ -2,6 +2,7 @@
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://sportsonline.gl/prog.txt");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+link = "https://sportsonline.gl/prog.txt";
 $result = curl_exec($ch);
 
 curl_close($ch);
